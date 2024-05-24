f74a785ed12d7684c87525ccae9d9ad3bac6c73c
JoostLocher <waterballoost@gmail.com>
2024-05-24 15:07:36 +0200

    (zoek pagina, nav partial, style.css) aangemaakt
    Voor views/partials/nav.ejs kijk onderstaande link
    https://www.digitalocean.com/community/tutorials/how-to-use-ejs-to-template-your-node-application#step-2-creating-the-ejs-partials

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/', (req, res) => {
     res.send('Test home page')
 })
 
+app.get('/zoek', (req, res) => {
+    res.render('pages/zoek')
+})
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
 })
