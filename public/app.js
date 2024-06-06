ecd79d1b563183b1fb2edc12c3cadf2540e3e89e
Annika <annika.mekkelholt@gmail.com>
2024-06-06 19:34:30 +0200

    changed width card etc

--- public/app.js
+++ public/app.js
@@ -32,33 +32,32 @@ document.addEventListener('DOMContentLoaded', function() {
     let dislikeBtn = document.querySelector('.dislike-btn');
     let likeBtn = document.querySelector('.like-btn');
     let playBtn = document.querySelector('.play-btn');
-    
+
     function loadSong(song) {
         card.querySelector('.card header').style.backgroundImage = `url(${song.cover})`;
         card.querySelector('section div h3').textContent = song.title;
         card.querySelector('section div p').textContent = song.artist;
         card.querySelector('aside p').textContent = song.genre;
     }
-    
+
     function handleSwipe(offsetX) {
         if (offsetX > 50 || offsetX < -50) {
             let liked = offsetX > 50;
             console.log(liked ? 'Liked' : 'Disliked', songs[currentSongIndex]);
-            
+
             currentSongIndex = (currentSongIndex + 1) % songs.length;
             loadSong(songs[currentSongIndex]);
-            
+
             card.style.transition = 'none';
             card.style.transform = 'translateX(0) rotate(0)';
             setTimeout(() => {
                 card.style.transition = 'transform 0.3s ease';
             }, 50);
-            
+
             dislikeBtn.style.backgroundColor = '';
             dislikeBtn.style.border = '';
             likeBtn.style.backgroundColor = '';
             likeBtn.style.border = '';
-
         } else {
             card.style.transform = 'translateX(0) rotate(0)';
 
@@ -127,15 +126,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     dislikeBtn.addEventListener('click', () => {
-        handleSwipe(-101);
+        dislikeBtn.style.backgroundColor = '#f19f99';
+        dislikeBtn.style.border = '.1em #c0271c solid';
+        setTimeout(() => handleSwipe(-101), 200);
     });
 
     likeBtn.addEventListener('click', () => {
-        handleSwipe(101); 
+        likeBtn.style.backgroundColor = '#a2daa7';
+        likeBtn.style.border = '.1em #1cc02a solid';
+        setTimeout(() => handleSwipe(101), 200);
     });
 
     playBtn.addEventListener('click', () => {
-      
         console.log('Play song:', songs[currentSongIndex]);
     });
 });
