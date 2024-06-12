4cf87f4a04340cfaf702c67968949dceb8c53d1f
Sep Duijkersloot <Sep.duijkersloott@gmail.com>
2024-06-12 16:13:57 +0200

    Quick backend fix

--- server.js
+++ server.js
@@ -454,9 +454,9 @@ function hasPreviewUrl(track) {
 
 app.get('/recommendations', async (req, res) => {
     try {
-        // const topTracks = await getTopTracks(req);
-        // const seedUri = topTracks.map((track) => track.id);
-        const seedUri = ['2lXqwlG8za1sWKgHRwEiEC', '0jsXpJsdXhnwnwnCLKjYLF']
+        const topTracks = await getTopTracks(req);
+        const seedUri = topTracks.map((track) => track.id);
+        // const seedUri = ['2lXqwlG8za1sWKgHRwEiEC', '0jsXpJsdXhnwnwnCLKjYLF']
         const limit = 2;
         const seed_type = 'seed_tracks'
         const recommendedTracks = await getRecommendations(req, seedUri, limit, seed_type);
@@ -485,9 +485,9 @@ app.get('/search-recommendations', async (req, res) => {
 
 app.get('/new-recommendation', async (req, res) => {
     try {
-        // const topTracks = await getTopTracks(req);
-        // const seedTracks = topTracks.map((track) => track.id);
-        const seedTracks = ['2lXqwlG8za1sWKgHRwEiEC', '0jsXpJsdXhnwnwnCLKjYLF']
+        const topTracks = await getTopTracks(req);
+        const seedTracks = topTracks.map((track) => track.id);
+        // const seedTracks = ['2lXqwlG8za1sWKgHRwEiEC', '0jsXpJsdXhnwnwnCLKjYLF']
         const limit = 1;
         const recommendedTracks = await getRecommendations(req, seedTracks, limit);
         res.json({ recommendation: recommendedTracks[0] });
@@ -504,9 +504,9 @@ app.get('/', async (req, res) =>
     {
         if (req.session.loggedIn) {
             try {
-                // const topTracks = await getTopTracks(req);
-                // const seedTracks = topTracks.map((track) => track.id);
-                const seedTracks = ['2lXqwlG8za1sWKgHRwEiEC', '0jsXpJsdXhnwnwnCLKjYLF']
+                const topTracks = await getTopTracks(req);
+                const seedTracks = topTracks.map((track) => track.id);
+                // const seedTracks = ['2lXqwlG8za1sWKgHRwEiEC', '0jsXpJsdXhnwnwnCLKjYLF']
                 const limit = 2;
                 const recommendedTracks = await getRecommendations(req, seedTracks, limit);
                 res.render('pages/verkennen', { 
